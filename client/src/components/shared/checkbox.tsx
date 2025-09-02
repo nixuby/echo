4c658f6c000114ffdcc8e2a1cbd34ce8823cf67c
Nixuby <152711547+nixuby@users.noreply.github.com>
2025-09-02 16:49:11 +0300

    Hide the checkmark if checkbox is not checked

--- client/src/components/shared/checkbox.tsx
+++ client/src/components/shared/checkbox.tsx
@@ -9,13 +9,13 @@ export default function CheckBox({ children, ...props }: CheckBoxProps) {
     return (
         <label className='flex items-center justify-start'>
             <input type='checkbox' className='peer hidden' {...props} />
-            <div className='flex gap-1 peer-checked:[&>div:first-child]:bg-indigo-600 peer-checked:[&>div:first-child]:hover:bg-indigo-500'>
+            <div className='peer-checked:[&_.-icon]:opacity-100! flex gap-1 peer-checked:[&>div:first-child]:bg-indigo-600 peer-checked:[&>div:first-child]:hover:bg-indigo-500'>
                 <div
                     className={clsx(
                         'cursor-pointer border border-gray-800 bg-gray-900 p-1 transition hover:border-gray-700 hover:bg-gray-800',
                     )}
                 >
-                    <CheckIcon className='size-4' />
+                    <CheckIcon className='-icon size-4 opacity-0 transition-opacity' />
                 </div>
                 <div>{children}</div>
             </div>
