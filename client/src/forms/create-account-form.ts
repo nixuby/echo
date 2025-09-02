47a98d38b4d391f26158e6b33d2beda035f1df81
Nixuby <152711547+nixuby@users.noreply.github.com>
2025-09-02 17:31:25 +0300

    Add the create account form

---
+++ client/src/forms/create-account-form.ts
@@ -0,0 +1,37 @@
+import z from 'zod';
+
+export type CreateAccountForm = {
+    username: string;
+    password: string;
+    confirm: string;
+    tos: boolean;
+};
+
+export const createAccountFormSchema = z
+    .object({
+        username: z
+            .string()
+            .min(3, 'Username must be at least 3 characters long')
+            .max(32, 'Username must be at most 32 characters long'),
+        password: z
+            .string()
+            .min(6, 'Password must be at least 6 characters long')
+            .max(128, 'Password must be at most 128 characters long'),
+        confirm: z.string().nonempty('Please confirm your password'),
+        tos: z.boolean(),
+    })
+    .refine((data) => data.password === data.confirm, {
+        message: 'Passwords must match',
+        path: ['confirm'],
+    })
+    .refine((data) => data.tos, {
+        message: 'You must accept the Terms of Service',
+        path: ['tos'],
+    });
+
+export const CREATE_ACCOUNT_FORM_DEFAULT: CreateAccountForm = {
+    username: '',
+    password: '',
+    confirm: '',
+    tos: false,
+};
--- client/src/routes/createaccount.tsx
+++ client/src/routes/createaccount.tsx
@@ -1,5 +1,132 @@
+import { useForm } from 'react-hook-form';
 import AuthLayout from '../components/auth/auth-layout';
+import TextBox from '../components/shared/textbox';
+import Button from '../components/shared/button';
+import { Link, useNavigate } from 'react-router';
+import { useEffect, useState } from 'react';
+import {
+    CREATE_ACCOUNT_FORM_DEFAULT,
+    createAccountFormSchema,
+    type CreateAccountForm,
+} from '../forms/create-account-form';
+import CheckBox from '../components/shared/checkbox';
+import { zodResolver } from '@hookform/resolvers/zod';
+import z from 'zod';
 
-export default function CreateAccountRoute() {
-    return <AuthLayout title='Create Account'>Cr</AuthLayout>;
+async function mockCreateAccount(
+    body: CreateAccountForm,
+): Promise<{ ok: true } | { ok: false; errors: Record<string, string> }> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const valRes = createAccountFormSchema.safeParse(body);
+
+            if (valRes.success) {
+                resolve({ ok: true });
+            } else {
+                const result = {
+                    ok: false,
+                    errors: {} as Record<string, string>,
+                };
+
+                const errorTree = z.treeifyError(valRes.error)
+                    .properties as Record<
+                    string,
+                    { errors: string[] } | undefined
+                >;
+                for (const err in errorTree) {
+                    if (errorTree[err] === undefined) continue;
+                    result.errors[err] = errorTree[err].errors[0];
+                }
+
+                resolve(result);
+            }
+        }, 500);
+    });
+}
+
+export default function SignInRoute() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isDirty, isSubmitted },
+        setError,
+        watch,
+        trigger,
+    } = useForm<CreateAccountForm>({
+        defaultValues: CREATE_ACCOUNT_FORM_DEFAULT,
+        resolver: zodResolver(createAccountFormSchema),
+    });
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const navigate = useNavigate();
+
+    const passwordValue = watch('password');
+
+    useEffect(() => {
+        if (isDirty && isSubmitted) trigger('confirm');
+    }, [passwordValue]);
+
+    function submitForm(data: CreateAccountForm) {
+        setIsLoading(true);
+
+        mockCreateAccount(data).then((response) => {
+            setIsLoading(false);
+
+            if (response.ok) {
+                navigate('/');
+            } else {
+                for (const field in response.errors) {
+                    setError(field as keyof CreateAccountForm, {
+                        message: response.errors[field],
+                    });
+                }
+            }
+        });
+    }
+
+    return (
+        <AuthLayout title='Sign In'>
+            <form
+                action=''
+                onSubmit={handleSubmit(submitForm)}
+                className='flex flex-col gap-4'
+            >
+                <TextBox
+                    label='Username'
+                    error={errors.username?.message}
+                    {...register('username')}
+                />
+                <TextBox
+                    label='Password'
+                    type='password'
+                    error={errors.password?.message}
+                    {...register('password')}
+                />
+                <TextBox
+                    label='Confirm'
+                    type='password'
+                    error={errors.confirm?.message}
+                    {...register('confirm')}
+                />
+                <CheckBox error={errors.tos?.message} {...register('tos')}>
+                    I agree to the{' '}
+                    <Link
+                        to='/tos'
+                        className='text-gray-400 transition hover:text-gray-300 hover:underline'
+                    >
+                        Terms of Service
+                    </Link>
+                </CheckBox>
+                {errors.root && (
+                    <div className='border border-red-400/40 bg-red-400/20 px-4 py-2 text-sm text-red-400'>
+                        {errors.root.message}
+                    </div>
+                )}
+                <Button submit disabled={isLoading}>
+                    Sign In
+                </Button>
+            </form>
+        </AuthLayout>
+    );
 }
