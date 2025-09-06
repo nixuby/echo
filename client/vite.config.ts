a1b04c3fe49e91320fb7523c3b44986985b138fc
Nixuby <152711547+nixuby@users.noreply.github.com>
2025-09-06 20:25:22 +0300

    Add "@/*" path alias to "./src/*"

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
+            '@': path.resolve(__dirname, 'src'),
             '@shared': path.resolve(__dirname, '../shared'),
         },
     },
