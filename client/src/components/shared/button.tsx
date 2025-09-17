7fa897714c26040684bf45b7c111e187776ee152
Nixuby <152711547+nixuby@users.noreply.github.com>
2025-09-17 17:56:18 +0300

    Add button types (primary, secondary, danger)

--- client/src/components/shared/button.tsx
+++ client/src/components/shared/button.tsx
@@ -2,9 +2,10 @@ import clsx from 'clsx/lite';
 
 export type ButtonProps = {
     submit?: boolean;
+    type?: 'primary' | 'secondary' | 'danger';
     size?: 'small' | 'normal' | 'large';
     children?: React.ReactNode;
-} & React.ComponentProps<'button'>;
+} & Omit<React.ComponentProps<'button'>, 'type'>;
 
 const BUTTON_SIZES = {
     small: 'text-sm px-4 py-1',
@@ -12,11 +13,18 @@ const BUTTON_SIZES = {
     large: 'text-lg px-4 py-3',
 } as const;
 
+const BUTTON_TYPES = {
+    primary: 'bg-indigo-800 hover:bg-indigo-700 border-indigo-600',
+    secondary: 'bg-gray-800 hover:bg-gray-700 border-gray-600',
+    danger: 'bg-red-600 hover:bg-red-500 border-red-400',
+};
+
 export default function Button({
     children,
     submit,
     className,
     size = 'normal',
+    type = 'primary',
     ...props
 }: ButtonProps) {
     return (
@@ -24,8 +32,9 @@ export default function Button({
             type={submit ? 'submit' : 'button'}
             role='button'
             className={clsx(
-                'cursor-pointer border border-indigo-600 bg-indigo-800 transition-colors hover:bg-indigo-700 disabled:border-gray-600 disabled:bg-gray-500',
+                'cursor-pointer border text-white transition-colors disabled:border-gray-600 disabled:bg-gray-500',
                 BUTTON_SIZES[size],
+                BUTTON_TYPES[type],
                 className,
             )}
             {...props}
