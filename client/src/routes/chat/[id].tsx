a63828d9e960a08b740d82ddf66b79c9444cb5ba
Nixuby <152711547+nixuby@users.noreply.github.com>
2025-09-19 15:23:56 +0300

    Change scrollbar style

--- client/src/routes/chat/[id].tsx
+++ client/src/routes/chat/[id].tsx
@@ -40,7 +40,7 @@ export default function ChatPage() {
                 className='relative flex h-[calc(100vh_-_3rem)] flex-col justify-between'
             >
                 <TitleBar>Chat</TitleBar>
-                <div className='grow overflow-y-auto'>
+                <div className='_scrollbar-thin grow overflow-y-auto'>
                     <div className='flex grow flex-col'>
                         <div className='flex flex-col gap-2 px-4 py-2'>
                             {data?.map((msg) => (
